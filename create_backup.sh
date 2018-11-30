1cb17706ef1452aa92a21be1479e981d56443f13
Somprasong Damyos <somprasong@hospital-os.com>
2018-11-30 16:30:20 +0700

    update script

--- create_backup.sh
+++ create_backup.sh
@@ -1,23 +1,29 @@
 echo "Script Backup PostgreSQL"
 echo "By Somprasong Damyos <somprasong.damyos@gmail.com>"
 echo " Create Apr 4,2018"
-pgHome=/var/lib/postgresql/10
-pgPath=$pgHome/bin
-echo -n "Please enter your database's name : ";read DB
+PG_HOME=/var/lib/postgresql/10
+PG_BIN=$PG_HOME/bin
+echo -n "Please enter your database's host : ";read DB_HOST
+echo -n "Please enter your database's port : ";read DB_PORT
+echo -n "Please enter your database's username : ";read DB_USER
+echo -n "Please enter your database's password : ";read DB_PWD
+echo -n "Please enter your database's name : ";read DB_NAME
+echo -n "Please enter your google drive backup directory : ";read GG_BACKUP
 #Backup
-pgBak=/backup
-mkdir -p $pgBak/GoogleDrive
-cp google-oauth2.sh /backup/GoogleDrive
-cp upload.sh /backup/GoogleDrive
-cp uploadGG.sh /backup/GoogleDrive
-cd $pgBak/GoogleDrive
-echo "#!/bin/sh" > daily_GoogleDrive.sh
-echo "/bin/rm -rf $pgBak/Daily/*.sql.gz" >> daily_GoogleDrive.sh
-echo "$pgPath/pg_dump -U postgres $DB | gzip -9 > $pgBak/GoogleDrive/\$(date +%Y%m%d).sql.gz" >> daily_GoogleDrive.sh
-echo "/bin/sh /backup/GoogleDrive/uploadGG.sh" >> daily_GoogleDrive.sh
+BACKUP_DIR=~/backup
+mkdir -p $BACKUP_DIR
+cp google-oauth2.sh $BACKUP_DIR
+cp upload.sh $BACKUP_DIR
+cp start.sh $BACKUP_DIR
+cd $BACKUP_DIR
+echo "#!/bin/sh" > daily_backup.sh
+echo "/bin/rm -rf $BACKUP_DIR/daily/*.sql.gz" >> daily_backup.sh
+echo "$PG_BIN/pg_dump -U $DB_USER -h $DB_HOST -p $DB_PORT $DB_NAME | gzip -9 > $BACKUP_DIR/daily/\$(date +%Y%m%d).sql.gz" >> daily_backup.sh
+echo "/bin/sh $BACKUP_DIR/upload.sh $BACKUP_DIR/daily/*.sql.gz $GG_BACKUP" >> daily_backup.sh
 chmod 755 *
-chown postgres. $pgBak
-echo "Database backup files are in : $pgBak"
-echo "Run cd /backup/GoogleDrive/google-drive-upload-master/;./google-oauth2.sh"
-echo "Run cd /backup/GoogleDrive/;./daily_GoogleDrive.sh"
-echo "Create Crontab User postgres|00 21 * * * /backup/GoogleDrive/daily_GoogleDrive.sh"
+chown $DB_USER $BACKUP_DIR
+echo "Database backup files are in : $BACKUP_DIR/daily"
+echo "Next step:"
+echo "Run cd $BACKUP_DIR;./google-oauth2.sh"
+echo "Run cd $BACKUP_DIR;./daily_backup.sh"
+echo "And create crontab User $DB_USER|23 30 * * * $BACKUP_DIR/daily_backup.sh"
